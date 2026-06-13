3d82dbfec7d0be84ab30ee121ae806b346f9576c
Naofel-Badsha <naofelbadsha@gmail.com>
2026-06-13 20:57:47 +0600

    added categoriesData

--- commerce-hub-client/src/Pages/Home/Home.jsx
+++ commerce-hub-client/src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "./Banner"
+import Categories from "./Categories"
 import SeasonCollection from "./SeasonCollection"
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <div>
       <Banner />
       <SeasonCollection />
+      <Categories />
     </div>
   )
 }
